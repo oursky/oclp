0c1e954258218b449ed96785e84e454e7e333f8c
Ben Lei <ben181231@gmail.com>
2014-06-12 16:17:30 +0800

    #4 Finish drafting "Create Message" page

--- js/message-create.js
+++ js/message-create.js
@@ -1,7 +1,24 @@
 (function(){
     var app = angular.module('messageCreate', []);
-    app.controller('MsgCreateController', function($scope){
-        
+    app.controller('MsgCreateController', function($scope, $http){
+        $scope.model = {};
+        $scope.submit = function(){
+            var data = {};
+            data.field1 = $scope.model.field1;
+            data.field2 = $scope.model.field2;
+            data.author = $scope.model.author;
+
+            $scope.model.isLoading = true;
+            //TODO: POST Message Data
+            alert('TODO - post data: ' + JSON.stringify(data));
+            $http.post('/create', data)
+                .success(function(data, status, headers, config){
+                    alert('Success: data - ' + JSON.stringify(data));
+                })
+                .error(function(data, status, headers, config){
+                    alert('Error: status - ' + status);
+                    $scope.model.isLoading = false;
+                });
+        };
     });
-    
 })();
