2e521591aebfa5b1606673caef951dbab2f16588
Rick Mak <rick.mak@gmail.com>
2014-06-13 23:35:34 +0800

    Contenteditable by chpapa

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,41 @@
+(function() {
+    angular.module('contenteditableDirectives', []).
+      directive('contenteditable', function () {
+          return {
+              restrict: 'A', // only activate on element attribute
+              require: '?ngModel', // get a hold of NgModelController
+              link: function (scope, element, attrs, ngModel) {
+                  if (!ngModel) return; // do nothing if no ng-model
+
+                  // Specify how UI should be updated
+                  ngModel.$render = function () {
+                      element.html(ngModel.$viewValue || '');
+                  };
+
+                  // Listen for change events to enable binding
+                  element.on('blur keyup change', function () {
+                      scope.$apply(readViewText);
+                  });
+
+                  // No need to initialize, AngularJS will initialize the text based on ng-model attribute
+
+                  // Write data to the model
+                  function readViewText() {
+                      var html = element.html();
+                      // When we clear the content editable the browser leaves a <br> behind
+                      // If strip-br attribute is provided then we strip this out
+                      if (attrs.stripBr && html == '<br>') {
+                          html = '';
+                      }
+                      ngModel.$setViewValue(html);
+                  }
+              }
+          };
+      });
+})();
+
 (function(){
-    var app = angular.module('oclp', ['ngRoute', 'messageStream', 'messageCreate', 'messagePage']);
+    var app = angular.module('oclp', ['ngRoute', 'messageStream', 'messageCreate', 'messagePage', 'contenteditableDirectives']);
     app.config(function($routeProvider, $locationProvider) {
         $locationProvider.html5Mode(true);
         $routeProvider
@@ -32,6 +68,7 @@
         $('body').removeClass('white');
 
         $scope.submit = function(){
+            console.log("SUBMIT");
             var data = {};
             data.field1 = $scope.model.field1;
             data.field2 = $scope.model.field2;
